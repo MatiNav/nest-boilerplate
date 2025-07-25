73af8d5cd8e458d84f802bbeaf8c6fd85bb9fe33
MatiNav <matiasnavarroezequiel@gmail.com>
2025-07-25 12:44:15 -0300

    add config files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Environment } from './config/env';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ load: [Environment] })],
   controllers: [AppController],
   providers: [AppService],
 })
